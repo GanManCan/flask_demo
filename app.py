9d2d2b6a18224170eea99193fd743888cbf19793
GanManCan <mgannon1606@gmail.com>
2024-07-22 22:37:42 -0400

    Add basic templates

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import re
 from datetime import datetime
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -10,18 +11,13 @@ def home():
     return "Hello, Flask!"
 
 @app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
 
-    # Filter the name argument to lettrs only using regular expressions.
-
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-    
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
